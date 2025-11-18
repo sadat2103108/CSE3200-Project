c3ea50f3abacbc6a28d49070adfc1d4a142c64e2
sadat <shameemsadat00@gmail.com>
2025-11-18 13:12:49 +0600

    first commit

---
+++ server/server.js
@@ -0,0 +1,16 @@
+import express from "express";
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+
+
+app.get("/", (req, res) => {
+  res.send("Server is running...");
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
